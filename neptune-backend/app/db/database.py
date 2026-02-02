6e886beac3a6bcddea0affadc838d70580d3d8a3
Aditya Suresh <152752311+adi-suresh01@users.noreply.github.com>
2026-02-01 22:40:13 -0500

    db changes

--- neptune-backend/app/db/database.py
+++ neptune-backend/app/db/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 import sys
 from app.core.settings import settings
 import logging
+from app.db.models import Base
 
 logger = logging.getLogger(__name__)
 
@@ -70,9 +70,6 @@ else:
 
 # Create session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Create declarative base for models
-Base = declarative_base()
 
 # Dependency for database sessions
 def get_db():
